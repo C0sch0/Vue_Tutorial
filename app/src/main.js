1fd49cda877c2ddadfb5d478c7daa9d874e3e970
Diego Iruretagoyena <diiru@uc.cl>
2019-01-02 12:06:45 -0300

    Global Base Components

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import AppDate from '@/components/AppDate'
+
+Vue.component('AppDate', AppDate)
 
 Vue.config.productionTip = false
 
@@ -10,6 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  components: { App }
 })
